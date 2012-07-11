23a289cbda9db90f5739facc3ba0a5bf3dfca4a1
Jeff Carpenter <hello@jeffcarp.com>
2012-07-10 18:49:27 -0700

    Maiden voyage.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shanghai::Application.config.secret_token = '8972e528c186fe3aa8e3348c8e9a615be021e3d3de5c8c99d38eb8d48c595341683bbc027329f267d5e2c51efdd6fb5b045a1cac79128065042d410a705380c8'
