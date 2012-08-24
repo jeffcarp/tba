ffa4db096d8646f1c0f1b194abd261dd4ba4a9fb
Jeff Carpenter <hello@jeffcarp.com>
2012-08-23 21:47:38 -0700

    Trying something.

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "523258854979.apps.googleusercontent.com", "0VbmjODTXmO0FtDmvtofWEBN", {access_type: 'online', approval_prompt: '', redirect_uri: "http://localhost:3000/google_oauth2/callback"}
+  # provider :google_oauth2, "523258854979.apps.googleusercontent.com", "0VbmjODTXmO0FtDmvtofWEBN", {access_type: 'online', approval_prompt: '', redirect_uri: "http://localhost:3000/google_oauth2/callback"}
 end
 
 OmniAuth.config.on_failure do |env|
