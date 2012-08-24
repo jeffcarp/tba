45ce9dc474cf8f346e1d473f11c029b6c9dde349
Jeff Carpenter <hello@jeffcarp.com>
2012-08-23 22:13:16 -0700

    Once again, human error.

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,8 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :google_oauth2, "523258854979.apps.googleusercontent.com", "0VbmjODTXmO0FtDmvtofWEBN", {access_type: 'online', approval_prompt: '', redirect_uri: "http://localhost:3000/google_oauth2/callback"}
+  provider :google_oauth2, "523258854979.apps.googleusercontent.com", "0VbmjODTXmO0FtDmvtofWEBN", {access_type: 'online', approval_prompt: '', redirect_uri: "http://localhost:3000/google_oauth2/callback"}
 end
 
 OmniAuth.config.on_failure do |env|
   [302, {'Location' => '/', 'Content-Type'=> 'text/html'}, []]
 end
-
--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google, "523258854979.apps.googleusercontent.com", "0VbmjODTXmO0FtDmvtofWEBN"
-end
