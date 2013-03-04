16af05c234ffcb99e9166fc9f7c9b79871203975
Jeff Carpenter <gcarpenterv@gmail.com>
2013-03-03 19:47:49 -0800

    More stats updates

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -13,6 +13,36 @@ class StatsController < ApplicationController
     @upcoming_issue = Issue.upcoming_issue
     @upcoming_posts = Post.find(:all, joins: [:issue, :user], conditions: ['issue_id = ?', @upcoming_issue.id], order: 'users.karma DESC')
     @current_issue = Issue.current_issue
+    @posts_per_issue = ActiveRecord::Base.connection.execute "select issues.publish_date as publish_date, count(posts.id) as post_count from issues left outer join posts on posts.issue_id = issues.id group by issues.publish_date order by issues.publish_date desc"
+puts @posts_per_issue
+
+    posts_per_issue = []
+    x_axis_labels = []
+    index = 0
+    min = false 
+    max = false 
+    cumulative_total = 0
+    @posts_per_issue.each_with_index do |issue, index|
+      x_axis_labels << Date.parse(issue['publish_date']).strftime("%b %-d") if index % 20 == 0
+      posts_per_issue << issue['post_count']
+
+      puts issue['post_count']
+
+      min = issue['post_count'] if !min
+      max = issue['post_count'] if !max
+      min = issue['post_count'] if issue['post_count'] < min
+      max = issue['post_count'] if issue['post_count'] > max
+    end
+    x_axis_labels.reverse!
+    posts_per_issue.reverse!
+    puts min
+    puts max
+
+    @posts_per_issue_chart = Gchart.line(:size => '600x300',
+      :axis_with_labels => 'x,y',
+      :axis_labels => [x_axis_labels.join('|'), "#{min}|#{(max+min)/4}|#{(max+min)/2}|#{((max+min)*(0.75)).to_i}|#{max}"],
+      :bg => '00000000',
+      :data => posts_per_issue)
   end
 
   def email
@@ -73,7 +103,7 @@ class StatsController < ApplicationController
     max = 0
     cumulative_total = 0
     @users.each do |k,v|
-      x_axis_labels << Date.parse(k).strftime("%b %-d") if index % 10 == 0
+      x_axis_labels << Date.parse(k).strftime("%b %-d") if index % 10 == 0 && k
       cumulative_total += v
       opens_data << cumulative_total
       index += 1
