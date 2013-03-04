bbcfdd3564f1807999bf66125685b74587965cbb
Jeff Carpenter <gcarpenterv@gmail.com>
2013-03-03 20:19:55 -0800

    I literally cannot think of anything else

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -38,10 +38,10 @@ puts @posts_per_issue
     puts min
     puts max
 
-    max = max
+    max = max.to_i
     top_quarter = ((max+min)*(0.75)).to_i
-    half = (max+min).to_i/2
-    bottom_quarter = (max+min).to_i/4
+    half = ((max+min).to_i/2).to_i
+    bottom_quarter = ((max+min).to_i/4).to_i
 
     @posts_per_issue_chart = Gchart.line(:size => '600x300',
       :axis_with_labels => 'x,y',
