b5574df5f452edc5274122a13e736c221c7f5d7e
Jeff Carpenter <gcarpenterv@gmail.com>
2013-03-17 22:21:34 -0700

    Sprucing up compose page

--- app/assets/javascripts/compose.js
+++ app/assets/javascripts/compose.js
@@ -50,26 +50,12 @@ $(function(){
 
   // Cancel photo
   $('.cancel_photo').on('click', function() {
-    $('.filepicker').show();
-    $('.photo_drag_drop').html('Or drop files here');
     var photo_url = $('input.photo_url');
     photo_url.val('');
     update_preview();
+    $(this).hide();
   });
 
 
-  var handleFP = function(event) {
-    $('.filepicker').hide();
-
-    var file = event.fpfile;
-      uploaded_photo = $('.uploaded_photo');
-      uploaded_photo.children('img').attr('src', file.url+"/convert?w=600");
-      uploaded_photo.show();
-
-      $('input.photo_url').val(file.url);
-  }
-
-  // Load non-blocking filepicker API
-  (function(a){if(window.filepicker){return}var b=a.createElement("script");b.type="text/javascript";b.async=!0;b.src=("https:"===a.location.protocol?"https:":"http:")+"//api.filepicker.io/v1/filepicker.js";var c=a.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c);var d={};d._queue=[];var e="pick,pickMultiple,read,write,writeUrl,export,convert,store,storeUrl,remove,stat,setKey,constructWidget,makeDropPane".split(",");var f=function(a,b){return function(){b.push([a,arguments])}};for(var g=0;g<e.length;g++){d[e[g]]=f(e[g],d._queue)}window.filepicker=d})(document);
 
 });
