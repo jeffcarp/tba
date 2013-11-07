27589b732ca33eaf855d9c6f94f084bbd5c30fe5
Jeff Carpenter <gcarpenterv@gmail.com>
2013-11-06 21:01:06 -0800

    Improve layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,2 @@
 module ApplicationHelper
-
-  def current_controller?(name)
-    return 'current' if controller.controller_name == name
-  end
-
-  def underline(path)
-    if request.env['PATH_INFO'] == path
-      return 'current'
-    else
-      return ' '
-    end
-  end
-
 end
