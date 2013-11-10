cd7a4b32a6643ca117f945c660a72ab94732fd22
Jeff Carpenter <gcarpenterv@gmail.com>
2013-11-09 16:07:39 -0800

    Remove integration tests from old version

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -7,24 +7,4 @@ describe 'Integration Routes' do
     response.should be_success
   end
 
-  it 'GET /today' do
-    get '/today'
-    response.should be_success
-  end
-
-  it 'GET /tommorrow' do
-    get '/tomorrow'
-    response.status.should == 302 
-  end
-
-  it 'GET /compose' do
-    get '/compose'
-    response.should be_success
-  end
-
-  it 'GET /guide' do
-    get '/guide'
-    response.should be_success
-  end
-
 end
